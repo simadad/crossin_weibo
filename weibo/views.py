b112b28cc72399f41b0fcdff9c023613c5c0a9ee
simadad <simadad@sina.com>
2017-11-10 16:54:30 +0800

    app account

--- weibo/views.py
+++ weibo/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
-def root(request):
-    return HttpResponse('HELLO WORLD!')
+@login_required
+def homepage(request):
+    user = request.user
+    return HttpResponse(str(user))
